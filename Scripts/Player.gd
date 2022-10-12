2cad009c6c4bf41a3122ec17fec5634ec355c4ac
guilhermepaulozup <fotkurz@gmail.com>
2022-10-12 20:50:53 -0300

    fix jumping

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,23 +1,26 @@
 extends KinematicBody2D
 
-# move in x axys and can jump (move on y axis)
 
-var velocity = Vector2.ZERO # the currently velocity of the object
-var move_speed = 480 # the moving speed
-var gravity = 100 # the gravity strength
-export var jump_force = 200 # the jumping strength
+export var gravity = 600
+export var walk_speed = 200
+export var jump_force = 500
 
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	velocity.y += delta * gravity
 	
-	# checks the move direction
-	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -walk_speed
+	elif Input.is_action_pressed("move_right"):
+		velocity.x = walk_speed
+	else:
+		velocity.x = 0
+		# smoothen the stop
+		# velocity.x = lerp(velocity.x, 0, 0.1)
 	
-	print(is_on_floor())
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		print(velocity.y)
-		velocity.y -= jump_force
-		
-	velocity.x = move_speed * move_direction
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+	 
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
-	move_and_slide(velocity) # adding the velocity of our object to kinematicbody.move_and_slide.
